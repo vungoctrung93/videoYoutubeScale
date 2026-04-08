18c330fff3cfb3693ce5a0edaf48fdff79cec3af
vungoctrung93 <vungoctrung93@gmail.com>
2026-04-08 14:16:46 +0700

    init video.xcodeproj

---
+++ video/videoApp.swift
@@ -0,0 +1,17 @@
+//
+//  videoApp.swift
+//  video
+//
+//  Created by Trung on 8/4/26.
+//
+
+import SwiftUI
+
+@main
+struct videoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
